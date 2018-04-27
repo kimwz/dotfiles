bd3cce497848bbf56cb322619ead8f2aa6e423b4
Karl Woojung Kim <kimwz@conbus.net>
2018-04-27 11:10:31 +0900

    Update installzsh.sh

--- installzsh.sh
+++ installzsh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o - https://raw.githubusercontent.com/kimwz/kimwz-oh-my-zsh-theme/master/install.sh | zsh
 chsh -s /bin/zsh
